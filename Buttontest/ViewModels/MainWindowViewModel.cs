5d52652028cd40b6a0014f3728eb28e7d3fd629d
lary <f98f96@gmail.com>
2020-12-09 17:20:37 +0300

    Initial commit

---
+++ Buttontest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Buttontest.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private string _text;
+        private bool _isEnabled;
+
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                IsEnabled = !string.IsNullOrEmpty(value);
+                this.RaiseAndSetIfChanged(ref _text, value);
+            }
+        }
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set => this.RaiseAndSetIfChanged(ref _isEnabled, value);
+        }
+    }
+}
